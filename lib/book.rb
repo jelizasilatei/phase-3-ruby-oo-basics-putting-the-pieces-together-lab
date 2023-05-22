6167d84f3f9dc4d7cb47c63ec62d4acfbe18a027
jelizasilatei <silateijc90@gmail.com>
2023-05-22 11:11:50 +0300

    Puttting pieces together

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,26 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title 
+    def initialize(title)
+      @title = title
+    end
 
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
 
+  end
+
+  # Create an instance of the Book class with only the title
+book = Book.new("And Then There Were None")
+book.author = "Agatha Christie"
+book.page_count = 272
+book. genre = "Mystery"
+
+puts book.title
+puts book.author
+puts book.page_count
+puts book.genre
+
+# Print the output of turn_page to the terminal
+ puts book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,32 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+      @brand = brand
+    end
+
+    def cobble
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end
+  end
+
+  # Create an instance of the Shoe class with property values
+shoe = Shoe.new("Nike")
+shoe.color = "Red"
+shoe.size = 9.5
+shoe.material = "Suede"
+shoe.condition = "tattered"
+
+# Output the properties to the terminal
+puts "Brand: #{shoe.brand}"
+puts "Color: #{shoe.color}"
+puts "Size: #{shoe.size}"
+puts "Material: #{shoe.material}"
+puts "Condition: #{shoe.condition}"
+
+# Invoke the cobble method
+shoe.cobble
+puts "Updated Condition: #{shoe.condition}"
